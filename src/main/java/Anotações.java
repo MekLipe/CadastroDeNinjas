6ea2634aafe75d18fd2cb768e33cc323910e4f72
MekLipe <lipe_kopper@hotmail.com>
2025-08-18 11:34:00 -0400

    ➕  build: Adiciona a dependencia JPA e cria a entidade Ninja no BD

---
+++ src/main/java/Anotações.java
@@ -0,0 +1,22 @@
+public @interface Anotações {
+    /*
+    * Anotações úteis sobre Spring:
+    *
+    *
+    *
+    *
+    * Anotações úteis sobre Git:
+    * -Remove o último commit:
+    * git reset --soft HEAD~1
+    * -Desfaz o commit e retira os arquivos do staging:
+    * git reset HEAD~1
+    * -Ve todos os commits recentes com mensagens:
+    * git log --oneline
+    * -Ve commits prontos para push:
+    * git cherry -v
+    *
+    *
+    *
+    *
+    */
+}
